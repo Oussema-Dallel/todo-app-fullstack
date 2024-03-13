c1219efb4f0d134d46a9b80483afc75085298bfc
Oussema Dallel <oussemadallel@gmail.com>
2024-03-13 15:02:48 +0100

    feat: wrap-up pg-db integration

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -1,11 +1,12 @@
 import { Pool } from 'pg';
+import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from '../utils/envSetup';
 
 const pool = new Pool({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'todofullstack',
-	password: '19922',
-	port: 5432,
+	user: DB_USER,
+	host: DB_HOST,
+	database: DB_NAME,
+	password: DB_PASS,
+	port: DB_PORT,
 });
 
 export default pool;
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,11 @@ import { asyncWrapper } from './utils/asyncWrapper';
 import cors from 'cors';
 import express from 'express';
 import type { Express } from 'express';
+import { PORT } from './utils/envSetup';
 import type { Todo } from './types/Todo';
 import { createTodo, deleteTodo, getTodos, updateTodo } from './services/todos';
 
 const app: Express = express();
-const port = 4000;
 
 // TODO: Add CORS options
 const corsOptions: cors.CorsOptions = {
@@ -25,6 +25,6 @@ app.put('/todos/:id', asyncWrapper<Todo>(updateTodo));
 
 app.delete('/todos/:id', asyncWrapper(deleteTodo));
 
-app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+	console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
